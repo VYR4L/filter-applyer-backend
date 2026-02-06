8726ff07a384984c3baa0f8ac2bdc5089f58a32e
VYR4L <fkz.zanatt@gmail.com>
2026-02-05 22:14:30 -0300

    fix: build

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ load_dotenv()
 class Settings:
     """Configurações da aplicação."""
     APP_HOST: str = os.getenv("APP_HOST", "0.0.0.0")
-    APP_PORT: int = int(os.getenv("APP_PORT", 8000))
+    APP_PORT: int = int(os.getenv("PORT", os.getenv("APP_PORT", 8000)))
     DEBUG: bool = os.getenv("DEBUG", "False").lower() == "true"
 
     match DEBUG:
